5ead2004b747e907a42c66774aa955d24d7c36dc
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-30 05:04:43 +0500

    function

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-let i = [..."chapter_8"]
-let upper = ''
-i.forEach(item => upper += item.toUpperCase())
-console.log(upper)
+// let i = [..."chapter_8"]
+// let upper = ''
+// i.forEach(item => upper += item.toUpperCase())
+// console.log(upper)
+
+
+function sum(a=3, b=6){
+    return a + b
+}
+
+console.log(sum()) // 9
+
+
+const sum_2 = (a,b) => a+b
+console.log(sum_2(5,5)) // 10
+
+
+let x = {
+    name: 'humoyun',
+    age: 19
+}
+
+x.fun = sum() // f is not function
+console.log(x.fun) // 9
