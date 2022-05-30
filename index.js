39fd8f076e06c3be0462fb2056c8e2a5ba2fe2da
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-30 19:55:10 +0500

    obj and array in function

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function sum(a=3, b=6){
 console.log(sum()) // 9
 
 
-const sum_2 = (a,b) => a+b
+const sum_2 = (a=5,b=5) => a+b
 console.log(sum_2(5,5)) // 10
 
 
@@ -28,3 +28,15 @@ const upper = x => x.toUpperCase()
 
 x.upper = upper
 console.log(x.upper(x.name)) //HUMOYUN
+
+
+const array = (x, y) => {
+    return x[0] + y[0]
+} 
+console.log(array([2,3], [3,4])) // 5
+
+const obj = ({x, y}, obj) => {
+    return { x: y * obj, y: x * obj}
+} 
+
+console.log(obj({x:4, y:3}, 4)) // x = 12, y = 16 
